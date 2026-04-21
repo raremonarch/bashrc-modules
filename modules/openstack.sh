9aeb0abf4061c030e3f02041dcb00f6371700a58
David McKee <git-author.n4t20@simplelogin.com>
2026-04-21 10:55:01 -0400

    openstack module

---
+++ modules/openstack.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Module: openstack
+# Version: 0.1.0
+# Description: OpenStack cloud management utilities
+# BashMod Dependencies: none
+
+export OS_CLOUD=ebsco-novelist
+export OS_REGION_NAME=SDCEDN
+
+osr() {
+  case "${1,,}" in
+    sdc)
+      export OS_REGION_NAME=SDC
+      ;;
+    sdcedn|edn)
+      export OS_REGION_NAME=SDCEDN
+      ;;
+    "")
+      printf 'Current region: %s\n' "$OS_REGION_NAME"
+      return 0
+      ;;
+    *)
+      printf 'Unknown region: %s\n' "$1"
+      printf 'Use: osr sdc | osr sdcedn\n'
+      return 1
+      ;;
+  esac
+
+  printf 'Region set to %s\n' "$OS_REGION_NAME"
+}
+
+os() {
+  openstack --os-cloud "$OS_CLOUD" --os-region-name "$OS_REGION_NAME" "$@"
+}
